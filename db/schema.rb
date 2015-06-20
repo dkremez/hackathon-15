fcaab6ee7fbe0e4ce3fb784d31baf290a2c6bfaf
Dzmitry Kremez <kremez19@gmail.com>
2015-06-20 16:13:53 +0300

    Add new serializers

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,6 @@ ActiveRecord::Schema.define(version: 20150620122016) do
     t.text     "description"
     t.string   "discount"
     t.string   "link"
-    t.string   "address"
-    t.string   "type"
-    t.float    "latitude"
-    t.float    "longitude"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
