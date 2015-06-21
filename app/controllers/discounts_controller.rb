e1b4a7174ec38c60f87a05a1a710bd4c59439603
Dzmitry Kremez <kremez19@gmail.com>
2015-06-21 06:54:48 +0300

    Highlight new discounts

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,7 +1,7 @@
 class DiscountsController < ApplicationController
 
   def index
-    @discounts = Discount.includes(:category, :addresses).order(:title).all
+    @discounts = Discount.includes(:category, :addresses).order(title: :asc).all
     render json: @discounts
   end
 
