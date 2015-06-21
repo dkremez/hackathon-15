740917e2906e4dca71b307aff4945c9741d04b23
Maks Kremez <mr.kremez@gmail.com>
2015-06-21 03:42:06 +0300

    finish with admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620203408) do
+ActiveRecord::Schema.define(version: 20150620233147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,19 +81,20 @@ ActiveRecord::Schema.define(version: 20150620203408) do
   add_index "discounts", ["category_id"], name: "index_discounts_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",               default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "username",               default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "access_token"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["access_token"], name: "index_users_on_access_token", using: :btree
